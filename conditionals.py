df7a178177a74e10866b5b08018f3d713caa7da0
frdrck100 <142630802+frdrck100@users.noreply.github.com>
2023-08-29 21:59:58 +0000

    Update conditionals.py

--- conditionals.py
+++ conditionals.py
@@ -1,6 +1,7 @@
 print("\n")
 country = input("Which country are you from?: ")
-if country == "Canada":
+if country.lower() == "Canada":
     print("Oh you realy look a Canadian")
 else:
     print("Oh, that's why you don't look a Canadian")
+ 

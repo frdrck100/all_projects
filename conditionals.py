f7f56aafbe4c52981c1ab9429fe4124c1ec3cb3c
frdrck100 <142630802+frdrck100@users.noreply.github.com>
2023-08-29 21:44:22 +0000

    Create conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,6 @@
+print("\n")
+country = input("Which country are you from?: ")
+if country == "Canada":
+    print("Oh you realy look a Canadia")
+else:
+    print("Oh, that's why you don't look a Canadian")
